6f7fbf3f49d32339c46c3d9fb83993d5612a1540
karmpatel <Karmpatel960@gmail.com>
2024-09-03 23:33:02 +0530

    route added

---
+++ src/app/api/payment/route.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from '@prisma/client';
+import { NextResponse } from 'next/server';
+
+const prisma = new PrismaClient();
+
+export async function GET() {
+    
+  try {
+    const events = await prisma.event.findMany({
+      include: {
+        images: true,
+        videos: true,
+        gallery: true,
+      },
+    });
+    console.log('Fetched events:', events); // Debugging output
+    return NextResponse.json(events);
+  } catch (error) {
+    console.error('Error fetching events:', error); // Log the error
+    return NextResponse.json({ error: 'Failed to fetch events' }, { status: 500 });
+  }
+}
